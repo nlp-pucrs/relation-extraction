66e292f8459b8d63d62a6c45b9131b680aee2a83
Bernardo Consoli <bernardo.consoli@acad.pucrs.br>
2018-08-14 15:00:28 -0300

    Fixed argument parser.

--- relp++.py
+++ relp++.py
@@ -4,18 +4,18 @@ import crf
 import argparse
 
 parser = argparse.ArgumentParser(description="Relp++ relation extractor")
-parser.add_argument("-fg", "--featuregenerator", help="Gera features para uso nos geradores de modelo e testes.")
-parser.add_argument("-crfgen", "--crfmodelgenerator", nargs="?", const="combo", help="Gera um modelo de aprendizado de máquina CRF utilizando features indicadas.")
-parser.add_argument("-ftr", "--featurestrain", help="Indica local de features para treino já existentes.")
-parser.add_argument("-fts", "--featurestest", help="Indica local de features para teste já existentes.")
-parser.add_argument("-crf", help="Extrai relações dos textos")
+parser.add_argument("-fg", "--featuregenerator", metavar=('FORMATED_TEXT'), help="Gera features para uso nos geradores de modelo e testes.")
+parser.add_argument("-crfgen", "--crfmodelgenerator", metavar=('FEATURES'), help="Gera um modelo de aprendizado de máquina CRF utilizando features indicadas.")
+#parser.add_argument("-ftr", "--featurestrain", help="Indica local de features para treino já existentes.")
+#parser.add_argument("-fts", "--featurestest", metavar=('FEATURES'), help="Indica local de features para teste já existentes.")
+parser.add_argument("-crf", nargs=2, metavar=('FEATURES', 'MODEL'), help="Extrai relações dos textos")
 
 args = parser.parse_args()
 
 try:
 	if args.featuregenerator:
 		feature_generator.generate_features(args.featuregenerator)
-		print("Features para treino criadas com sucesso.")
+		print("Features criadas com sucesso.")
 except:
 	print("\nErro na criação de features. Checar se argumento de entrada está no formato correto.\n")
 	traceback.print_exc(file=sys.stdout)
@@ -23,32 +23,18 @@ except:
 
 try:
 	if args.crfmodelgenerator:
-		if args.featurestrain:
-			crf.train_crf(args.featurestrain)
-			print("Modelo CRF gerados com sucesso.")
-		elif args.featuregenerator:
-			crf.train_crf('features_treino.txt.gz')
-			print("Modelo CRF gerados com sucesso.")
-		elif args.crfmodelgenerator != "combo":
-			feature_generator.generate_features(args.crfmodelgenerator)
-			crf.train_crf('features_treino.txt.gz')
-			os.remove('features_treino.txt.gz')
-			print("Modelo CRF gerados com sucesso.")
-		else:
-			sys.exit(1)
+			crf.train_crf(args.crfmodelgenerator)
+			print("Modelo CRF gerado com sucesso.")
 except:
-	print("\nErro na criação do modelo CRF. Checar se argumento de entrada está no formato correto.\n")
+	print("\nErro na gereção do modelo CRF. Checar se argumento de entrada está no formato correto.\n")
 	traceback.print_exc(file=sys.stdout)
 	sys.exit(1)
 
 try:
 	if args.crf:
-		if args.featurestest:
-			crf.run_crf(args.featurestest, args.crf)
+			crf.run_crf(args.crf[0], args.crf[1])
 			print("Resultados utilizando modelo CRF gerados com sucesso.")
-		else:
-			sys.exit(1)
 except:
-	print("\nErro na criação do modelo CRF. Checar se argumento de entrada está no formato correto.\n")
+	print("\nErro na geração de resultados para modelo CRF. Checar se argumento de entrada está no formato correto.\n")
 	traceback.print_exc(file=sys.stdout)
 	sys.exit(1)
